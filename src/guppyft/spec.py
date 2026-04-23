bb066ac820ac948c0fdeeef7e644a1b379abe2df
Maximilian Rüsch <maximilian.ruesch@quantinuum.com>
2026-04-23 09:35:38 +0200

    Add docstrings

--- src/guppyft/spec.py
+++ src/guppyft/spec.py
@@ -10,8 +10,15 @@ from hugr.passes.composable import ComposablePass
 @dataclass(frozen=True, kw_only=True)
 class EncoderSpec:
     logical_ops: dict[tuple[str, str], GuppyFunctionDefinition[Any, Any]]
+    """The operations to encode / replace with the given functions. Keys are tuples
+    (namespace, name) for the operation to replace."""
     setup: GuppyFunctionDefinition[[], None]
+    """Called before the computational program."""
     teardown: GuppyFunctionDefinition[[], None]
+    """Called after the computational program."""
     tket_passes: list[ComposablePass]
+    """Additional tket passes to run on the computational program."""
     wrapper_extensions: list[Extension] = field(default_factory=list)
+    """Extensions required to (de)serialize programs using setup/teardown."""
     libs: list[Package] = field(default_factory=list)
+    """Additional libraries required to run the encoded program."""
