6727f9ef34b16f360fdfb2652865a3ebba548a82
Henry Semenenko <henry.semenenko@quantinuum.com>
2026-04-29 14:28:18 +0100

    pull globals from test PR

---
+++ src/guppyft/globals.py
@@ -0,0 +1,106 @@
+from collections.abc import Callable
+from typing import Any, cast, no_type_check
+
+from guppylang import guppy
+from guppylang.std.builtins import owned
+from guppylang_internals.compiler.core import EXTENSION_OPS_WITH_SIDE_EFFECTS
+from guppylang_internals.decorator import hugr_op
+from guppylang_internals.tys.arg import Argument
+from guppylang_internals.tys.arg import TypeArg as GuppyTypeArg
+from guppylang_internals.tys.common import ToHugrContext
+from guppylang_internals.tys.subst import Inst
+from guppylang_internals.tys.ty import NoneType, TupleType
+from hugr import ops
+from hugr import tys as ht
+from hugr.tys import ListArg, TypeArg, TypeBound, TypeTypeArg
+from tket_exts import globals
+
+T = guppy.type_var("T", copyable=False, droppable=False)
+
+IN = guppy.type_var("IN")
+OUT = guppy.type_var("OUT")
+
+# Mark ops as having side effects to add order edges in the HUGR
+# when calls return None.
+# https://github.com/Quantinuum/guppylang/issues/1698
+EXTENSION_OPS_WITH_SIDE_EFFECTS.append("tket.globals.with")
+EXTENSION_OPS_WITH_SIDE_EFFECTS.append("tket.globals.map")
+
+
+def with_op_for_global_var(
+    var_name: str,
+) -> Callable[[ht.FunctionType, Inst, ToHugrContext], ops.DataflowOp]:
+    def op(concrete: ht.FunctionType, args: Inst, ctx: ToHugrContext) -> ops.DataflowOp:
+        global_arg = cast("TypeTypeArg", args[0].to_hugr(ctx))
+
+        if global_arg.ty.type_bound() != TypeBound.Linear:
+            raise TypeError(f"Global arg must be linear. Found {global_arg.ty}.")
+
+        return globals.with_def.instantiate(
+            [ht.StringArg(var_name), global_arg, ListArg([]), ListArg([])],
+            concrete,
+        )
+
+    return op
+
+
+@hugr_op(with_op_for_global_var("GUPPY_FT_GLOBAL"))
+@no_type_check
+def with_global_state(new_value: T @ owned, func: Callable[[], None]) -> T: ...
+
+
+def _unpack_tuple_arg(arg: Argument, ctx: ToHugrContext) -> list[TypeArg]:
+    match arg:
+        case GuppyTypeArg(ty=gty):
+            match gty:
+                case TupleType(args=elems):
+                    return [ty.to_hugr(ctx) for ty in elems]
+                case NoneType():
+                    return []
+                case _:
+                    return [arg.to_hugr(ctx)]
+        case _:
+            return [arg.to_hugr(ctx)]
+
+
+def _map_op_for_global_var(
+    var_name: str,
+) -> Callable[[ht.FunctionType, Inst, ToHugrContext], ops.DataflowOp]:
+    def op(concrete: ht.FunctionType, args: Inst, ctx: ToHugrContext) -> ops.DataflowOp:
+        global_arg = args[0].to_hugr(ctx)
+        if len(args) == 2:
+            input_args = ListArg([])
+            # There is a mismatch in function signatures between the Guppy compiler
+            # and the HUGR Op instantiation. The Guppy compile unpacks tuples at
+            # the output of functions while HUGR does not. It is necessary for us
+            # to manually unpack the tuple type here for the signatures to match.
+            output_args = ListArg(_unpack_tuple_arg(args[1], ctx))
+        else:
+            input_args = ListArg([args[1].to_hugr(ctx)])
+            output_args = ListArg(_unpack_tuple_arg(args[2], ctx))
+
+        return globals.map_def.instantiate(
+            [
+                ht.StringArg(var_name),
+                global_arg,
+                input_args,
+                output_args,
+            ],
+            concrete,
+        )
+
+    return op
+
+
+@hugr_op(_map_op_for_global_var("GUPPY_FT_GLOBAL"))
+@no_type_check
+def map_global_state_input(func: Callable[[T, IN], OUT], inputs: IN) -> OUT: ...
+
+
+@hugr_op(_map_op_for_global_var("GUPPY_FT_GLOBAL"))
+@no_type_check
+def map_global_state_no_input(func: Callable[[T], OUT]) -> OUT: ...
+
+
+@guppy.overload(map_global_state_input, map_global_state_no_input)
+def map_global_state(*args: Any) -> Any: ...
---
+++ tests/test_global.py
@@ -0,0 +1,200 @@
+import re
+from typing import no_type_check
+
+import pytest
+from guppylang import array, guppy
+from guppylang.emulator import EmulatorError
+from guppylang.std.builtins import result
+from guppylang.std.quantum import discard, measure, qubit, x
+
+from guppyft.globals import map_global_state, with_global_state
+
+
+def test_with() -> None:
+    @guppy
+    def foo(qb: qubit, i: tuple[int, int]) -> int:
+        x(qb)
+        return i[0] + i[1]
+
+    @guppy
+    def my_prog() -> None:
+        i = map_global_state(foo, (9, 10))
+        result("my_prog", i)
+
+    @guppy
+    def main() -> None:
+        qb = qubit()
+        qb = with_global_state(qb, my_prog)
+        result("qb_main", measure(qb))
+
+    res = main.emulator(n_qubits=1).run().collated_shots()
+    assert res == [
+        {
+            "qb_main": [1],
+            "my_prog": [19],
+        }
+    ]
+
+
+def test_map_no_input() -> None:
+    @guppy
+    def foo(qb: qubit) -> int:
+        x(qb)
+        return 19
+
+    @guppy
+    def my_prog() -> None:
+        i = map_global_state(foo)
+        result("my_prog", i)
+
+    @guppy
+    def main() -> None:
+        qb = qubit()
+        qb = with_global_state(qb, my_prog)
+        result("qb_main", measure(qb))
+
+    res = main.emulator(n_qubits=1).run().collated_shots()
+    assert res == [
+        {
+            "qb_main": [1],
+            "my_prog": [19],
+        }
+    ]
+
+
+def test_map_return_none() -> None:
+    @guppy
+    def foo(qb: qubit) -> None:
+        x(qb)
+        result("foo", 19)
+
+    @guppy
+    def my_prog() -> None:
+        map_global_state(foo)
+
+    @guppy
+    def main() -> None:
+        qb = qubit()
+        qb = with_global_state(qb, my_prog)
+        result("qb_main", measure(qb))
+
+    res = main.emulator(n_qubits=1).run().collated_shots()
+    assert res == [
+        {
+            "qb_main": [1],
+            "foo": [19],
+        }
+    ]
+
+
+def test_map_return_tuple() -> None:
+    @guppy
+    def foo(qb: qubit, i: tuple[int, int]) -> tuple[int, int]:
+        x(qb)
+        return i
+
+    @guppy
+    def my_prog() -> None:
+        i = map_global_state(foo, (9, 10))
+        result("my_prog", i[0] + i[1])
+
+    @guppy
+    def main() -> None:
+        qb = qubit()
+        qb = with_global_state(qb, my_prog)
+        result("qb_main", measure(qb))
+
+    res = main.emulator(n_qubits=1).run().collated_shots()
+    assert res == [
+        {
+            "qb_main": [1],
+            "my_prog": [19],
+        }
+    ]
+
+
+def test_map_without_with() -> None:
+    @guppy
+    def foo(qb: qubit) -> None:
+        x(qb)
+
+    @guppy
+    def main() -> None:
+        map_global_state(foo)
+
+    with pytest.raises(EmulatorError) as _:
+        main.emulator(n_qubits=1).run().collated_shots()
+
+
+def test_nested_with() -> None:
+    @guppy
+    @no_type_check
+    def foo(i: array[int, 1], j: int) -> None:
+        i[0] = i[0] + j
+
+    @guppy
+    def my_nested_prog() -> None:
+        map_global_state(foo, 1)
+
+    @guppy
+    def my_prog() -> None:
+        map_global_state(foo, 1)
+        arr_inner = array(10)
+        arr_inner = with_global_state(arr_inner, my_nested_prog)
+        result("arr_inner", arr_inner)
+        map_global_state(foo, 2)
+
+    @guppy
+    def main() -> None:
+        arr_outer = array(0)
+        arr_outer = with_global_state(arr_outer, my_prog)
+        result("arr_outer", arr_outer)
+
+    res = main.emulator(n_qubits=1).run().collated_shots()
+    assert res == [
+        {
+            "arr_inner": [[11]],
+            "arr_outer": [[3]],
+        }
+    ]
+
+
+def test_mismatch_type() -> None:
+    @guppy
+    @no_type_check
+    def foo(arr: array[int, 1]) -> int:
+        return arr[0]
+
+    @guppy
+    def my_prog() -> None:
+        map_global_state(foo)
+
+    @guppy
+    def main() -> None:
+        qb = qubit()
+        qb = with_global_state(qb, my_prog)
+        discard(qb)
+
+    with pytest.raises(
+        RuntimeError,
+        match=re.escape(
+            "Failed to emit LLVM for function tests.test_global.test_mismatch_type"
+            ".<locals>.my_prog at node Node(15)"
+        ),
+    ) as _:
+        main.emulator(n_qubits=1).run().collated_shots()
+
+
+def test_non_linear_global() -> None:
+    @guppy
+    def my_prog() -> None:
+        return
+
+    @guppy
+    def main() -> None:
+        with_global_state(0, my_prog)
+
+    with pytest.raises(
+        TypeError, match=r"Global arg must be linear. Found int<6>."
+    ) as _:
+        main.emulator(n_qubits=1).run().collated_shots()
