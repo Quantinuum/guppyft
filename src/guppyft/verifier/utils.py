8fea3b48dd24a452212d66ea4f5b1df3756baf6c
PabloAndresCQ <pablo.andresmartinez@quantinuum.com>
2026-07-20 13:28:11 +0100

    ArraySlicer added to utils

--- src/guppyft/verifier/utils.py
+++ src/guppyft/verifier/utils.py
@@ -1,6 +1,21 @@
+from typing import Generic, Self, no_type_check
+
 from selene_stim_plugin.state import Pauli, Phase, Stabilizer, StabilizerList
 from zixy.container.coeffs import Sign
 from zixy.qubit import pauli
+
+from guppylang import guppy
+from guppylang.defs import GuppyFunctionDefinition
+from guppylang.std.quantum import qubit
+from guppylang.std.builtins import array, comptime, owned
+from guppylang.std.collections import Queue, empty_queue
+from guppylang.std.option import nothing, some
+from guppylang.std.num import nat
+
+type SingleBlockUnitary = GuppyFunctionDefinition[[array[qubit, N]], None]  # type: ignore[valid-type]
+type DoubleBlockUnitary = GuppyFunctionDefinition[
+    [array[qubit, N], array[qubit, N]], None  # type: ignore[valid-type]
+]
 
 
 def _convert_pauli(selene_pauli: Pauli) -> pauli.PauliMatrix:
@@ -55,3 +70,38 @@ def stabilizerlist_to_signterms(stab_list: StabilizerList) -> pauli.SignTerms:
         term = selene_stabilizer_to_zixy_signterm(gen)
         sign_terms.append(term)
     return sign_terms
+
+
+N = guppy.nat_var("N")
+T = guppy.type_var("T", copyable=False, droppable=False)
+
+@guppy.struct
+@no_type_check
+class ArraySlicer(Generic[T, N]):
+    queue: Queue[T, N]
+
+    @guppy
+    @no_type_check
+    def discard_empty(self: Self @ owned) -> None:
+        self.queue.discard_empty()
+
+    @guppy
+    @no_type_check
+    def take(self, n: nat @ comptime) -> array[T, "n"]:
+        if n > len(self.queue):
+            exit("Cannot take more qubits than are available in the slicer.")
+        slice = array(nothing[T]() for _ in range(n))
+        for i in range(n):
+            slice[i].swap(some(self.queue.pop())).unwrap_nothing()
+
+        clean_slice = array(q.unwrap() for q in slice)
+        return clean_slice
+
+
+@guppy
+@no_type_check
+def array_slicer(arr: array[T, N] @ owned) -> ArraySlicer[T, N]:
+    queue = empty_queue[T, N]()
+    for q in arr:
+        queue.push(q)
+    return ArraySlicer(queue)
