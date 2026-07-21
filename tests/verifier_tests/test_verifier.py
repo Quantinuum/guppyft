d1aa0c7a46787f48ead1c485669de049004837c3
PabloAndresCQ <pablo.andresmartinez@quantinuum.com>
2026-07-21 14:47:34 +0100

    Added tests for non-CSS [[4,2,2]]

--- tests/verifier_tests/test_verifier.py
+++ tests/verifier_tests/test_verifier.py
@@ -14,7 +14,7 @@ from guppyft.verifier.verify import (
     identity_code,
 )
 
-from .ops import bitflip, iceberg, nonCSS_5q, steane
+from .ops import bitflip, iceberg, nonCSS_4q, nonCSS_5q, steane
 
 
 def test_bell_state_stabilizers() -> None:
@@ -484,3 +484,73 @@ def test_nonCSS_5q_cz_not_cx() -> None:
         nonCSS_5q.CODE_DEF,
     )
     assert sem != impl
+
+
+def test_single_block_nonCSS_4q_id() -> None:
+    sem, impl = compute_verification_signterms(
+        nonCSS_4q.logical_identity,
+        nonCSS_4q.physical_identity,
+        nonCSS_4q.CODE_DEF,
+    )
+    assert sem == impl
+
+
+def test_double_block_nonCSS_4q_id() -> None:
+    sem, impl = compute_verification_signterms_double_block(
+        nonCSS_4q.logical_identity_double_block,
+        nonCSS_4q.physical_identity_double_block,
+        nonCSS_4q.CODE_DEF,
+    )
+    assert sem == impl
+
+
+def test_nonCSS_4q_row1() -> None:
+    sem, impl = compute_verification_signterms(
+        nonCSS_4q.logical_row1,
+        nonCSS_4q.physical_row1,
+        nonCSS_4q.CODE_DEF,
+    )
+    assert sem == impl
+
+
+def test_nonCSS_4q_row2() -> None:
+    sem, impl = compute_verification_signterms(
+        nonCSS_4q.logical_row2,
+        nonCSS_4q.physical_row2,
+        nonCSS_4q.CODE_DEF,
+    )
+    assert sem == impl
+
+
+def test_nonCSS_4q_row3() -> None:
+    sem, impl = compute_verification_signterms(
+        nonCSS_4q.logical_row3,
+        nonCSS_4q.physical_row3,
+        nonCSS_4q.CODE_DEF,
+    )
+    assert sem == impl
+
+
+def test_nonCSS_4q_row4() -> None:
+    sem, impl = compute_verification_signterms(
+        nonCSS_4q.logical_row4,
+        nonCSS_4q.physical_row4_incorrect,
+        nonCSS_4q.CODE_DEF,
+    )
+    assert sem != impl
+
+    sem, impl = compute_verification_signterms(
+        nonCSS_4q.logical_row4,
+        nonCSS_4q.physical_row4_correct,
+        nonCSS_4q.CODE_DEF,
+    )
+    assert sem == impl
+
+
+def test_nonCSS_4q_intra_cz() -> None:
+    sem, impl = compute_verification_signterms(
+        nonCSS_4q.logical_intra_cz,
+        nonCSS_4q.physical_intra_cz,
+        nonCSS_4q.CODE_DEF,
+    )
+    assert sem == impl
