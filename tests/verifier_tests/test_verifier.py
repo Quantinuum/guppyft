65fc034f55475d8b692a85645a7d656f5b5f9820
PabloAndresCQ <pablo.andresmartinez@quantinuum.com>
2026-07-21 12:58:05 +0100

    Added tests for non-CSS [[5,1,3]]

--- tests/verifier_tests/test_verifier.py
+++ tests/verifier_tests/test_verifier.py
@@ -14,7 +14,7 @@ from guppyft.verifier.verify import (
     identity_code,
 )
 
-from .ops import bitflip, iceberg, steane
+from .ops import bitflip, iceberg, nonCSS_5q, steane
 
 
 def test_bell_state_stabilizers() -> None:
@@ -394,3 +394,93 @@ def test_iceberg_transversal_zzmax() -> None:
         iceberg.ICEBERG_DEF,
     )
     assert sem == impl
+
+
+def test_single_block_nonCSS_5q_id() -> None:
+    sem, impl = compute_verification_signterms(
+        nonCSS_5q.logical_identity,
+        nonCSS_5q.physical_identity,
+        nonCSS_5q.CODE_DEF,
+    )
+    assert sem == impl
+
+
+def test_double_block_nonCSS_5q_id() -> None:
+    sem, impl = compute_verification_signterms_double_block(
+        nonCSS_5q.logical_identity_double_block,
+        nonCSS_5q.physical_identity_double_block,
+        nonCSS_5q.CODE_DEF,
+    )
+    assert sem == impl
+
+
+def test_nonCSS_5q_k() -> None:
+    sem, impl = compute_verification_signterms(
+        nonCSS_5q.logical_k,
+        nonCSS_5q.physical_k,
+        nonCSS_5q.CODE_DEF,
+    )
+    assert sem == impl
+
+
+def test_nonCSS_5q_kdg() -> None:
+    sem, impl = compute_verification_signterms(
+        nonCSS_5q.logical_kdg,
+        nonCSS_5q.physical_kdg,
+        nonCSS_5q.CODE_DEF,
+    )
+    assert sem == impl
+
+
+def test_nonCSS_5q_different_k() -> None:
+    sem, impl = compute_verification_signterms(
+        nonCSS_5q.logical_k,
+        nonCSS_5q.physical_kdg,
+        nonCSS_5q.CODE_DEF,
+    )
+    assert sem != impl
+
+
+def test_nonCSS_5q_h() -> None:
+    sem, impl = compute_verification_signterms(
+        nonCSS_5q.logical_h,
+        nonCSS_5q.physical_h,
+        nonCSS_5q.CODE_DEF,
+    )
+    assert sem == impl
+
+
+def test_nonCSS_5q_k_not_h() -> None:
+    sem, impl = compute_verification_signterms(
+        nonCSS_5q.logical_h,
+        nonCSS_5q.physical_k,
+        nonCSS_5q.CODE_DEF,
+    )
+    assert sem != impl
+
+
+def test_nonCSS_5q_cz() -> None:
+    sem, impl = compute_verification_signterms_double_block(
+        nonCSS_5q.logical_cz,
+        nonCSS_5q.physical_cz,
+        nonCSS_5q.CODE_DEF,
+    )
+    assert sem == impl
+
+
+def test_nonCSS_5q_cx() -> None:
+    sem, impl = compute_verification_signterms_double_block(
+        nonCSS_5q.logical_cx,
+        nonCSS_5q.physical_cx,
+        nonCSS_5q.CODE_DEF,
+    )
+    assert sem == impl
+
+
+def test_nonCSS_5q_cz_not_cx() -> None:
+    sem, impl = compute_verification_signterms_double_block(
+        nonCSS_5q.logical_cz,
+        nonCSS_5q.physical_cx,
+        nonCSS_5q.CODE_DEF,
+    )
+    assert sem != impl
