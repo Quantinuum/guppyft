babff7678a3921da5bd37b1d0474588304477cf0
Callum Macpherson <93673602+CalMacCQ@users.noreply.github.com>
2026-08-05 13:58:19 +0100

    Update src/guppyft/verifier/verify.py
    Co-authored-by: Pablo Andres-Martinez <104848389+PabloAndresCQ@users.noreply.github.com>

--- src/guppyft/verifier/verify.py
+++ src/guppyft/verifier/verify.py
@@ -182,7 +182,7 @@ def compute_verification_signterms(
     semantic_choi_stabilizers = compute_stabilizers_single_block(
         identity_code(code_definition.num_logical_qubits),
         semantic_function,
-        code_definition.num_logical_qubits + num_ancilla_qubits,
+        2 * code_definition.num_logical_qubits + num_ancilla_qubits,
     )
 
     # Expand the 2k logical stabilizers to 2k stabilizers of size 2n.
