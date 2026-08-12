36d0a0f99dffa2b7b6bac7e7cab1e0c64ca21851
Maximilian Rüsch <maximilian.ruesch@quantinuum.com>
2026-08-12 10:15:08 +0200

    Get encoding spec for steane

--- src/guppyft/code/steane/encoder_spec.py
+++ src/guppyft/code/steane/encoder_spec.py
@@ -1,5 +1,6 @@
+from collections.abc import Mapping
 from dataclasses import dataclass
-from typing import no_type_check
+from typing import Any, no_type_check
 
 from guppylang import guppy
 from guppylang.defs import GuppyFunctionDefinition
@@ -23,6 +24,7 @@ from guppyft.code.steane.primitives import (
 )
 from guppyft.code.util import LogicalBlock, RawMeasurement
 from guppyft.encode import (
+    EncoderParams,
     EncoderSpec,
     ImplementOpsSpec,
     OpReplacements,
@@ -33,6 +35,14 @@ from guppyft.encode import (
 )
 from guppyft.extensions import std_ops, std_types, steane_ops, steane_types
 from guppyft.globals import map_global, with_global
+
+
+class SteaneEncoderParams(EncoderParams):
+    def encoding(self) -> str:
+        return "steane"
+
+    def params(self) -> Mapping[str, Any]:
+        return {}
 
 
 @dataclass
