63e0a557e42d02e9fec107881558a49469d72ae4
Henry Semenenko <40664241+hsemenenko@users.noreply.github.com>
2026-08-12 15:15:00 +0100

    feat: Add state factories and use for Steane zero state (#209)

--- src/guppyft/code/steane/encoder_spec.py
+++ src/guppyft/code/steane/encoder_spec.py
@@ -1,24 +1,25 @@
 from collections.abc import Mapping
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import Any, no_type_check
 
 from guppylang import guppy
 from guppylang.defs import GuppyFunctionDefinition
 from guppylang.library import GuppyLibrary, link_name
 from guppylang.std.builtins import array, comptime, owned
-from guppylang.std.collections import Stack
+from guppylang.std.collections import Stack, empty_queue
 from guppylang.std.option import Option, nothing, some
 from guppylang.std.platform import panic
 from hugr.ext import ExtensionRegistry
 from hugr.package import Package
 from hugr.std import _std_extensions
 
+from guppyft.code._state_factory import StateFactory
 from guppyft.code.steane.primitives import (
     cx,
     decode,
     h,
     measure_z,
-    prep_zero_non_ft,
+    prep_zero_ft,
     x,
     z,
 )
@@ -48,9 +49,27 @@ class SteaneEncoderParams(EncoderParams):
         return {"n_blocks": self.n_blocks}
 
 
+@dataclass(frozen=True)
+class RUSStateFactoryConf:
+    """Steane RUS state factory configuration.
+
+    Attributes:
+        size: Maximum number of states to be produced in parallel.
+        max_attempts: Maximum number of repeat-until-success attempts.
+    """
+
+    size: int
+    max_attempts: int
+
+
 @dataclass
 class SteaneSpec:
+    """Steane encoder spec"""
+
     n_blocks: int
+    zero_factory_conf: RUSStateFactoryConf = field(
+        default_factory=lambda: RUSStateFactoryConf(1, 5)
+    )
 
     def gen_implement_spec(self) -> ImplementOpsSpec:
         # TODO STATE should be generic for all codes. The methods that are code specific
@@ -61,6 +80,10 @@ class SteaneSpec:
         class STATE:
             blocks: array[Option[LogicalBlock[7]], comptime(self.n_blocks)]  # type: ignore[valid-type,type-arg]
             addr_stack: Stack[tuple[int, int], comptime(self.n_blocks)]  # type: ignore[valid-type,type-arg]
+
+            zero_state_factory: StateFactory[  # type: ignore[valid-type,type-arg]
+                7, 1, comptime(self.zero_factory_conf.size)
+            ]
 
             @guppy
             @no_type_check
@@ -103,12 +126,12 @@ class SteaneSpec:
         # See https://github.com/quantinuum-dev/guppyft/issues/161.
         @guppy
         @no_type_check
-        @link_name("guppyft.steane._prep_zero_non_ft")
-        def _prep_zero_non_ft() -> tuple[tuple[int, int]]:
+        @link_name("guppyft.steane._prep_zero")
+        def _prep_zero() -> tuple[tuple[int, int]]:
             @guppy
             def _impl(state: STATE @ owned) -> tuple[STATE, tuple[int, int]]:
                 blk_id, qb_id = state.allocate_next_addr()
-                blk = prep_zero_non_ft()
+                blk = state.zero_state_factory.get_state()
                 state.put_block(blk_id, blk)
                 return state, (blk_id, qb_id)
 
@@ -232,6 +255,12 @@ class SteaneSpec:
                     array(some((blk, 1)) for blk in range(comptime(self.n_blocks))),
                     comptime(self.n_blocks),
                 ),
+                # Zero state factory
+                StateFactory(
+                    prep_zero_ft,
+                    comptime(self.zero_factory_conf.max_attempts),
+                    empty_queue(),
+                ),
             )
 
         @guppy.declare
@@ -248,6 +277,8 @@ class SteaneSpec:
                 else:
                     blk.unwrap_nothing()
 
+            state.zero_state_factory.discard()
+
         def build_wrapper(
             func: GuppyFunctionDefinition[[], None],
         ) -> GuppyFunctionDefinition[[], None]:
@@ -263,7 +294,7 @@ class SteaneSpec:
         lib = GuppyLibrary.from_members(
             state_gen,
             state_discard,
-            _prep_zero_non_ft,
+            _prep_zero,
             _measure_z,
             _free,
             decode,
@@ -275,7 +306,7 @@ class SteaneSpec:
 
         ops = OpReplacements().with_generated_decls(
             {
-                ("guppyft.steane.ops", "prep_zero"): "guppyft.steane._prep_zero_non_ft",
+                ("guppyft.steane.ops", "prep_zero"): "guppyft.steane._prep_zero",
                 ("guppyft.steane.ops", "measure_z"): "guppyft.steane._measure_z",
                 ("guppyft.steane.ops", "free"): "guppyft.steane._free",
                 ("guppyft.steane.ops", "x"): "guppyft.steane._x",
--- tests/code/steane/test_steane_encode.py
+++ tests/code/steane/test_steane_encode.py
@@ -23,7 +23,7 @@ def test_encoder() -> None:
 
     pkg = main.compile()
     phys_pkg = SteaneSpec(n_blocks=1).encode(pkg)
-    res = EmulatorBuilder().build(phys_pkg, n_qubits=7).run().collated_shots()
+    res = EmulatorBuilder().build(phys_pkg, n_qubits=8).run().collated_shots()
 
     assert res == [{"res": [0]}]
 
