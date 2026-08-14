72f4b64fa611bd2e6d102e0f5f467e4b8948d4e1
Callum Macpherson <callum.macpherson@quantinuum.com>
2026-08-14 16:49:29 +0100

    refactor: use the new clifford checking interface for the test suite

--- tests/verifier_tests/test_verifier_nonCSS_4q.py
+++ tests/verifier_tests/test_verifier_nonCSS_4q.py
@@ -1,104 +1,102 @@
+import pytest
+
 from guppyft.verifier.verify import (
-    compute_verification_signterms_double_block_unitary,
-    compute_verification_signterms_single_block_state,
-    compute_verification_signterms_single_block_unitary,
+    InvalidImplementationError,
+    check_clifford_semantics,
+    check_stabilizer_state_semantics,
 )
 
 from .ops import nonCSS_4q
 
 
 def test_single_block_nonCSS_4q_id() -> None:
-    sem, impl = compute_verification_signterms_single_block_unitary(
+    check_clifford_semantics(
         nonCSS_4q.specify_identity,
         nonCSS_4q.implement_identity,
         nonCSS_4q.CODE_DEF,
     )
-    assert sem == impl
 
 
 def test_double_block_nonCSS_4q_id() -> None:
-    sem, impl = compute_verification_signterms_double_block_unitary(
+    check_clifford_semantics(
         nonCSS_4q.specify_identity_double_block,
         nonCSS_4q.implement_identity_double_block,
         nonCSS_4q.CODE_DEF,
     )
-    assert sem == impl
 
 
 def test_nonCSS_4q_zero_state() -> None:
-    sem, impl = compute_verification_signterms_single_block_state(
+    check_stabilizer_state_semantics(
         nonCSS_4q.specify_zero_state,
         nonCSS_4q.implement_non_ft_zero_state,
         nonCSS_4q.CODE_DEF,
     )
-    assert sem == impl
 
 
 def test_nonCSS_4q_plus_state() -> None:
-    sem, impl = compute_verification_signterms_single_block_state(
+    check_stabilizer_state_semantics(
         nonCSS_4q.specify_plus_state,
         nonCSS_4q.implement_non_ft_plus_state,
         nonCSS_4q.CODE_DEF,
     )
-    assert sem == impl
 
 
 def test_nonCSS_4q_zero_state_not_plus_state() -> None:
-    sem, impl = compute_verification_signterms_single_block_state(
-        nonCSS_4q.specify_zero_state,
-        nonCSS_4q.implement_non_ft_plus_state,
-        nonCSS_4q.CODE_DEF,
-    )
-    assert sem != impl
+    with pytest.raises(
+        InvalidImplementationError,
+        match=r"The implementation does not match the specified semantics.",
+    ):
+        check_stabilizer_state_semantics(
+            nonCSS_4q.specify_zero_state,
+            nonCSS_4q.implement_non_ft_plus_state,
+            nonCSS_4q.CODE_DEF,
+        )
 
 
 def test_nonCSS_4q_row1() -> None:
-    sem, impl = compute_verification_signterms_single_block_unitary(
+    check_clifford_semantics(
         nonCSS_4q.specify_row1,
         nonCSS_4q.implement_row1,
         nonCSS_4q.CODE_DEF,
     )
-    assert sem == impl
 
 
 def test_nonCSS_4q_row2() -> None:
-    sem, impl = compute_verification_signterms_single_block_unitary(
+    check_clifford_semantics(
         nonCSS_4q.specify_row2,
         nonCSS_4q.implement_row2,
         nonCSS_4q.CODE_DEF,
     )
-    assert sem == impl
 
 
 def test_nonCSS_4q_row3() -> None:
-    sem, impl = compute_verification_signterms_single_block_unitary(
+    check_clifford_semantics(
         nonCSS_4q.specify_row3,
         nonCSS_4q.implement_row3,
         nonCSS_4q.CODE_DEF,
     )
-    assert sem == impl
 
 
 def test_nonCSS_4q_row4() -> None:
-    sem, impl = compute_verification_signterms_single_block_unitary(
-        nonCSS_4q.specify_row4,
-        nonCSS_4q.implement_row4_incorrect,
-        nonCSS_4q.CODE_DEF,
-    )
-    assert sem != impl
-
-    sem, impl = compute_verification_signterms_single_block_unitary(
+    with pytest.raises(
+        InvalidImplementationError,
+        match=r"The implementation does not match the specified semantics.",
+    ):
+        check_clifford_semantics(
+            nonCSS_4q.specify_row4,
+            nonCSS_4q.implement_row4_incorrect,
+            nonCSS_4q.CODE_DEF,
+        )
+    check_clifford_semantics(
         nonCSS_4q.specify_row4,
         nonCSS_4q.implement_row4_correct,
         nonCSS_4q.CODE_DEF,
     )
-    assert sem == impl
 
 
 def test_nonCSS_4q_intra_cz() -> None:
-    sem, impl = compute_verification_signterms_single_block_unitary(
+    check_clifford_semantics(
         nonCSS_4q.specify_intra_cz,
         nonCSS_4q.implement_intra_cz,
         nonCSS_4q.CODE_DEF,
     )
-    assert sem == impl
--- tests/verifier_tests/test_verifier_steane.py
+++ tests/verifier_tests/test_verifier_steane.py
@@ -4,60 +4,51 @@ from guppyft.verifier.verify import (
     InvalidImplementationError,
     check_clifford_semantics,
     check_stabilizer_state_semantics,
-    compute_verification_signterms_double_block_state,
-    compute_verification_signterms_double_block_unitary,
-    compute_verification_signterms_single_block_state,
-    compute_verification_signterms_single_block_unitary,
 )
 
 from .ops import steane
 
 
 def test_steane_single_block_identity() -> None:
-    sem, impl = compute_verification_signterms_single_block_unitary(
+    check_clifford_semantics(
         steane.specify_identity,
         steane.implement_identity,
         code_definition=steane.STEANE_DEF,
     )
-    assert sem == impl
 
 
 def test_steane_single_block_identity_with_shor_extraction() -> None:
-    sem, impl = compute_verification_signterms_single_block_unitary(
+    check_clifford_semantics(
         steane.specify_identity,
         steane.implement_identity_with_shor_extraction,
         code_definition=steane.STEANE_DEF,
         num_ancilla_qubits=1,
     )
-    assert sem == impl
 
 
 def test_steane_double_block_identity_with_shor_extraction() -> None:
-    sem, impl = compute_verification_signterms_double_block_unitary(
+    check_clifford_semantics(
         steane.specify_identity_double_block,
         steane.implement_identity_double_block_with_shor_extraction,
         code_definition=steane.STEANE_DEF,
         num_ancilla_qubits=2,
     )
-    assert sem == impl
 
 
 def test_steane_double_block_identity() -> None:
-    sem, impl = compute_verification_signterms_double_block_unitary(
+    check_clifford_semantics(
         steane.specify_identity_double_block,
         steane.implement_identity_double_block,
         code_definition=steane.STEANE_DEF,
     )
-    assert sem == impl
 
 
 def test_steane_zero_state() -> None:
-    sem, impl = compute_verification_signterms_single_block_state(
+    check_stabilizer_state_semantics(
         steane.specify_zero_state,
         steane.implement_non_ft_zero_state,
         code_definition=steane.STEANE_DEF,
     )
-    assert sem == impl
     check_stabilizer_state_semantics(
         steane.specify_zero_state,
         steane.implement_non_ft_zero_state,
@@ -66,42 +57,35 @@ def test_steane_zero_state() -> None:
 
 
 def test_steane_ft_zero_state() -> None:
-    sem, impl = compute_verification_signterms_single_block_state(
+    check_stabilizer_state_semantics(
         steane.specify_zero_state,
         steane.implement_ft_zero_state,
         code_definition=steane.STEANE_DEF,
         num_ancilla_qubits=7,
     )
-    assert sem == impl
 
 
 def test_steane_plus_state() -> None:
-    sem, impl = compute_verification_signterms_single_block_state(
+    check_stabilizer_state_semantics(
         steane.specify_plus_state,
         steane.implement_non_ft_plus_state,
         code_definition=steane.STEANE_DEF,
     )
-    assert sem == impl
 
 
 def test_steane_zero_state_not_plus_state() -> None:
-    sem, impl = compute_verification_signterms_single_block_state(
-        steane.specify_zero_state,
-        steane.implement_non_ft_plus_state,
-        code_definition=steane.STEANE_DEF,
-    )
-
-    assert sem != impl
+    with pytest.raises(
+        InvalidImplementationError,
+        match=r"The implementation does not match the specified semantics.",
+    ):
+        check_stabilizer_state_semantics(
+            steane.specify_zero_state,
+            steane.implement_non_ft_plus_state,
+            code_definition=steane.STEANE_DEF,
+        )
 
 
 def test_steane_bell_state() -> None:
-    sem, impl = compute_verification_signterms_double_block_state(
-        steane.specify_bell_state,
-        steane.implement_non_ft_bell_state,
-        code_definition=steane.STEANE_DEF,
-    )
-    assert sem == impl
-
     check_stabilizer_state_semantics(
         steane.specify_bell_state,
         steane.implement_non_ft_bell_state,
@@ -110,63 +94,51 @@ def test_steane_bell_state() -> None:
 
 
 def test_steane_h() -> None:
-    sem, impl = compute_verification_signterms_single_block_unitary(
+    check_clifford_semantics(
         steane.specify_h,
         steane.implement_h,
         code_definition=steane.STEANE_DEF,
     )
 
-    assert sem == impl
-
 
 def test_steane_h_with_ancilla() -> None:
-    sem, impl = compute_verification_signterms_single_block_unitary(
+    check_clifford_semantics(
         steane.specify_h,
         steane.implement_h_with_ancilla,
         code_definition=steane.STEANE_DEF,
         num_ancilla_qubits=1,
     )
-    assert sem == impl
 
 
 def test_steane_s() -> None:
-    sem, impl = compute_verification_signterms_single_block_unitary(
+    check_clifford_semantics(
         steane.specify_s,
         steane.implement_s,
         code_definition=steane.STEANE_DEF,
     )
-
-    assert sem == impl
 
 
 def test_steane_sdg() -> None:
-    sem, impl = compute_verification_signterms_single_block_unitary(
+    check_clifford_semantics(
         steane.specify_sdg,
         steane.implement_sdg,
         code_definition=steane.STEANE_DEF,
     )
-
-    assert sem == impl
 
 
 def test_steane_invalid_s() -> None:
-    sem, impl = compute_verification_signterms_single_block_unitary(
-        steane.specify_s,
-        steane.implement_sdg,
-        code_definition=steane.STEANE_DEF,
-    )
-
-    assert sem != impl
+    with pytest.raises(
+        InvalidImplementationError,
+        match=r"The implementation does not match the specified semantics.",
+    ):
+        check_clifford_semantics(
+            steane.specify_s,
+            steane.implement_sdg,
+            code_definition=steane.STEANE_DEF,
+        )
 
 
 def test_steane_invalid_sdg() -> None:
-    sem, impl = compute_verification_signterms_single_block_unitary(
-        steane.specify_sdg,
-        steane.implement_s,
-        code_definition=steane.STEANE_DEF,
-    )
-
-    assert sem != impl
     with pytest.raises(
         InvalidImplementationError,
         match=r"The implementation does not match the specified semantics.",
@@ -179,13 +151,6 @@ def test_steane_invalid_sdg() -> None:
 
 
 def test_steane_cx() -> None:
-    sem, impl = compute_verification_signterms_double_block_unitary(
-        steane.specify_cx,
-        steane.implement_cx,
-        code_definition=steane.STEANE_DEF,
-    )
-    assert sem == impl
-
     check_clifford_semantics(
         steane.specify_cx,
         steane.implement_cx,
