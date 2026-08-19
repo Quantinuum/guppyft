e833d78b1d89eb49d647b9611cd9bb995449a852
Callum Macpherson <93673602+CalMacCQ@users.noreply.github.com>
2026-08-19 16:53:09 +0100

    docs: add sphinx API docs for guppyft (#226)

---
+++ docs/conf.py
@@ -0,0 +1,42 @@
+import guppyft  # noqa: INP001
+
+html_title = f"GuppyFT v{guppyft.__version__} Documentation"
+
+html_theme = "quantinuum_sphinx"
+html_theme_options = {
+    "sidebar_hide_name": False,
+}
+
+html_show_sourcelink = False
+html_copy_source = False
+
+templates_path = ["_templates"]
+
+master_doc = "index"
+author = "Quantinuum"
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.coverage",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.autosummary",
+    "myst_nb",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.intersphinx",
+    "quantinuum_sphinx",
+]
+
+
+# Sphinx autosummary
+# https://www.sphinx-doc.org/en/master/usage/extensions/autosummary.html
+
+# __all__ dictates which classes and functions are documented for a module
+autosummary_ignore_module_all = False  # Respect __all__ if specified
+
+
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3", None),
+    "hugr": ("https://quantinuum.github.io/hugr/", None),
+    "zixy": ("https://quantinuum.github.io/zixy/", None),
+    "guppylang": ("https://docs.quantinuum.com/guppy/", None),
+}
