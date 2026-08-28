c4cef87ecec5d62f003e31d810a8048f4ac1b58f
Maximilian Rüsch <maximilian.ruesch@quantinuum.com>
2026-08-28 17:29:12 +0100

    More superfluous changes

--- rust/src/bindings.rs
+++ rust/src/bindings.rs
@@ -1,7 +1,8 @@
+//! Supporting Rust module for the Python bindings.
+
 mod hugr;
 
 use pyo3::pymodule;
-
 /// Python module containing the Rust bindings.
 ///
 /// The definitions here should be reflected in the `src/guppyft/_bindings/__init__.pyi` type stubs.
