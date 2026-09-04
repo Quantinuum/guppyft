bbf5d488a71c8a79c7514fcb0dbd591c4fd42fe3
Maximilian Rüsch <maximilian.ruesch@quantinuum.com>
2026-09-04 16:14:22 +0100

    Remove unused definitions from verifier interface

--- src/guppyft/verifier/__init__.py
+++ src/guppyft/verifier/__init__.py
@@ -12,15 +12,12 @@ from .verify import (
 )
 
 __all__ = [
-    "CodeDefinitionError",
-    "StabilizerCode",
     "compute_stabilizers_double_block_unitary",
     "compute_stabilizers_single_block_unitary",
     "compute_verification_signterms_double_block_state",
     "compute_verification_signterms_double_block_unitary",
     "compute_verification_signterms_single_block_state",
     "compute_verification_signterms_single_block_unitary",
-    "identity_code",
     "selene_stabilizer_to_zixy_signterm",
     "stabilizerlist_to_signterms",
 ]
