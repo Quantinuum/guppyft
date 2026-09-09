347141cc59294702284e82779c5081d0c7f2323f
Callum Macpherson <93673602+CalMacCQ@users.noreply.github.com>
2026-09-09 12:54:23 +0100

    Update src/guppyft/decompose/_comparator_based_rz.py
    Co-authored-by: Alec Edgington <54802828+cqc-alec@users.noreply.github.com>

--- src/guppyft/decompose/_comparator_based_rz.py
+++ src/guppyft/decompose/_comparator_based_rz.py
@@ -222,7 +222,7 @@ class ComparatorBasedRz[
     Algorithm:
 
     1. Compute :math:`n = 1 + \\lceil \log_2(1/\varepsilon) \\rceil` and
-       :math:`k = 2^{n-1} + \\lfloor 2^{n-1} \tan(\\theta/2) + 1/2 \\rfloor`.
+       :math:`k = 2^{n-1} + \\lfloor 2^{n-1} \\tan(\\theta/2) + 1/2 \\rfloor`.
     2. Prepare register :math:`a` in superposition :math:`|+\rangle^{\\otimes n}`.
     3. Perform the comparison :math:`a \\geq k` on the target qubit.
     4. Apply an :math:`S` gate to the target qubit.
