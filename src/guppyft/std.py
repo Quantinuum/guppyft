090fe6c60fccf0a4731c5123f96c98f2851c15eb
Maximilian Rüsch <maximilian.ruesch@quantinuum.com>
2026-09-09 10:45:25 +0100

    Clean std

--- src/guppyft/std.py
+++ src/guppyft/std.py
@@ -8,35 +8,31 @@ from guppylang import guppy
 from guppylang.std.builtins import array
 from guppylang_internals.compiler.core import CompilerContext
 from guppylang_internals.decorator import custom_type, hugr_op
-from guppylang_internals.tys.arg import Argument, ConstArg
+from guppylang_internals.tys.arg import Argument
 from guppylang_internals.tys.common import ToHugrContext
 from guppylang_internals.tys.param import ConstParam
 from guppylang_internals.tys.subst import Inst
 from guppylang_internals.tys.ty import NumericType
+from hugr.ext import OpDef, TypeDef
 from hugr.ops import DataflowOp, ExtOp
 
 from guppyft.extensions import std_ops, std_types
 
-OPS_EXTN = std_ops()
-TYPES_EXTN = std_types()
 
-
-def _std_ty(
-    ty_name: str,
+def _instantiator_for_ty(
+    ty_def: TypeDef,
 ) -> Callable[[Sequence[Argument], CompilerContext], ht.Type]:
     def ty(args: Sequence[Argument], ctx: ToHugrContext) -> ht.Type:
-        [k_arg] = args
-        assert isinstance(k_arg, ConstArg)
-        return ht.ExtType(TYPES_EXTN.get_type(ty_name), [k_arg.to_hugr(ctx)])
+        return ht.ExtType(ty_def, [arg.to_hugr(ctx) for arg in args])
 
     return ty
 
 
-def _std_op(
-    op_name: str,
+def _instantiator_for_op(
+    op_def: OpDef,
 ) -> Callable[[ht.FunctionType, Inst, ToHugrContext], DataflowOp]:
     def op(ty: ht.FunctionType, inst: Inst, ctx: ToHugrContext) -> DataflowOp:
-        return ExtOp(OPS_EXTN.get_op(op_name), ty, [arg.to_hugr(ctx) for arg in inst])
+        return ExtOp(op_def, ty, [arg.to_hugr(ctx) for arg in inst])
 
     return op
 
@@ -45,7 +41,7 @@ N = guppy.nat_var("N")
 
 
 @custom_type(
-    _std_ty("logical_measurement"),
+    _instantiator_for_ty(std_types.logical_measurement_def),
     copyable=True,
     droppable=True,
     params=[ConstParam(1, "k", NumericType(NumericType.Kind.Nat))],
@@ -58,7 +54,7 @@ class LogicalMeasurement(Generic[N]):  # type: ignore[misc]
         return decode(self)
 
 
-@hugr_op(_std_op("decode"))
+@hugr_op(_instantiator_for_op(std_ops.decode_def))
 @no_type_check
 def decode(meas: LogicalMeasurement[N]) -> array[bool, N]:
     """Decode logical measurement"""
