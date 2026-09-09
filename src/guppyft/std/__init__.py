20b95c7863eba0e1318e2bf82c0e3a9ae039f08d
Henry Semenenko <40664241+hsemenenko@users.noreply.github.com>
2026-09-09 18:07:03 +0200

    docs: Fix links to `PreBlock` and `StateFactory` (#360)

--- src/guppyft/std/__init__.py
+++ src/guppyft/std/__init__.py
@@ -1,5 +1,6 @@
 """Standard types and operations shared between QEC architectures."""
 
+from . import state_factory
 from ._logical_block import LogicalBlock
 from ._measurement import LogicalMeasurement, decode
 
@@ -7,4 +8,5 @@ __all__ = [
     "LogicalBlock",
     "LogicalMeasurement",
     "decode",
+    "state_factory",
 ]
