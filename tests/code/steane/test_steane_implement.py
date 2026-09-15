f96d8f40dc6d0ff361b32be1f3999b00ff373ee0
Henry Semenenko <40664241+hsemenenko@users.noreply.github.com>
2026-09-15 11:29:13 +0100

    chore: Update guppylang to v1.1.1 (#325)

--- tests/code/steane/test_steane_implement.py
+++ tests/code/steane/test_steane_implement.py
@@ -16,7 +16,7 @@ def test_qalloc_measure() -> None:
 
     pkg = main.compile()
     phys_pkg = SteaneBuilder().build(n_blocks=1).implement_ops(pkg, as_bytes=True)
-    res = EmulatorBuilder().build(phys_pkg, n_qubits=8).run().collated_shots()  # type: ignore[arg-type]
+    res = EmulatorBuilder().build(phys_pkg, n_qubits=8).run().collated_shots()
 
     assert res == [{"res": [0]}]
 
@@ -31,7 +31,7 @@ def test_x() -> None:
 
     pkg = main.compile()
     phys_pkg = SteaneBuilder().build(n_blocks=1).implement_ops(pkg, as_bytes=True)
-    res = EmulatorBuilder().build(phys_pkg, n_qubits=8).run().collated_shots()  # type: ignore[arg-type]
+    res = EmulatorBuilder().build(phys_pkg, n_qubits=8).run().collated_shots()
 
     assert res == [{"res": [1]}]
 
@@ -48,7 +48,7 @@ def test_h_z() -> None:
 
     pkg = main.compile()
     phys_pkg = SteaneBuilder().build(n_blocks=1).implement_ops(pkg, as_bytes=True)
-    res = EmulatorBuilder().build(phys_pkg, n_qubits=8).run().collated_shots()  # type: ignore[arg-type]
+    res = EmulatorBuilder().build(phys_pkg, n_qubits=8).run().collated_shots()
 
     assert res == [{"res": [1]}]
 
@@ -65,7 +65,7 @@ def test_cx() -> None:
 
     pkg = main.compile()
     phys_pkg = SteaneBuilder().build(n_blocks=2).implement_ops(pkg, as_bytes=True)
-    res = EmulatorBuilder().build(phys_pkg, n_qubits=18).run().collated_shots()  # type: ignore[arg-type]
+    res = EmulatorBuilder().build(phys_pkg, n_qubits=18).run().collated_shots()
 
     assert res == [{"q0": [1], "q1": [1]}]
 
