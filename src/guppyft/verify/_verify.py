afbee151d97b16fb521abcb2c780d42c6d43d68f
Callum Macpherson <callum.macpherson@quantinuum.com>
2026-09-16 13:09:12 +0100

    refactor _get_num_func_qubits

--- src/guppyft/verify/_verify.py
+++ src/guppyft/verify/_verify.py
@@ -45,23 +45,16 @@ def _invoke_selene_stim(
 def _get_num_func_qubits(
     func: GuppyFunctionDefinition[Any, Any], num_qubits: int, num_ancilla_qubits: int
 ) -> int:
-    # If the user does not specify the impl_num_ancillas argument in the
-    # valid_clifford_implementation or valid_stabilizer_state_preparation,
-    # We check if the @expected_qubits metadata exists and adjust the number of qubits
-    # accordingly
-    if num_ancilla_qubits == 0:
-        try:
-            num_qubits = func.wrapped.metadata._node_metadata[  # type: ignore[attr-defined]
-                "tket.hint.expected_qubits"
-            ]
-        except KeyError:
-            num_qubits = num_qubits
-
-        return num_qubits
-    # If the user specifies an impl_num_ancillas argument this will override any
-    # @expected_qubits metadata.
-    else:
+    # If the user specifies an impl_num_ancillas argument in
+    # valid_clifford_implementation or valid_stabilizer_state_preparation this
+    #  will override any @expected_qubits metadata.
+    if num_ancilla_qubits > 0:
         return num_qubits + num_ancilla_qubits
+
+    metadata = func.wrapped.metadata._node_metadata  # type: ignore[attr-defined]
+    # Return the value specficed in the @expected_qubits metadata
+    # If metadata.get("tket.hint.expected_qubits") is None, "num_qubits" is the value.
+    return metadata.get("tket.hint.expected_qubits", num_qubits)
 
 
 def _compute_stabilizers_single_block_state(
